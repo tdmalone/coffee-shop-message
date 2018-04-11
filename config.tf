db4f873d6cc773eb3875d51d5e3b2a9ba7d61f6d
Tim Malone <tdmalone@gmail.com>
2018-04-12 05:54:25 +1000

    Clean Travis config + add Terraform config validation

--- config.tf
+++ config.tf
@@ -13,9 +13,8 @@
  * @see https://www.terraform.io/docs/configuration/providers.html#provider-versions
  */
 provider "aws" {
-  region = "ap-southeast-2"
-
-  #version = "~> 1.14"
+  region  = "ap-southeast-2"
+  version = "~> 1.14"
 }
 
 /**
